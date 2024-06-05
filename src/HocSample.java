e7c2564d7896d04382fce4d307539dec123d2a29
Paulo Rogerio <paulo@intranet>
2024-06-05 08:44:53 -0300

    Second version

--- src/HocSample.java
+++ src/HocSample.java
@@ -1,24 +1,21 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class HocSample {
 
   public static void main(String[] args) {
      var list = Arrays.asList("Orange", "Apple", "Banana", "Grape");
-        // we are passing an array and an anonymous inner class instance of FnFactory as arguments to mapForEach method.
-        var out = mapForEach(list, new FnFactory<String, Object>() {
-            @Override
-            public Object execute(final String it) {
-                return it.length();
-            }
-        });
-        System.out.println(out);
+         // we are passing the array and a lambda expression as arguments to mapForEach method.
+         var out = mapForEach(list, it -> it.length());
+         // This can be further simplified to "mapForEach(list, String::length);", I'm writing the expanded version for readability
+         System.out.println(out); // [6, 5, 6, 5]
   }
-    static <T, S> ArrayList<S> mapForEach(List<T> arr, FnFactory<T, S> fn) {
+     static <T, S> ArrayList<S> mapForEach(List<T> arr, Function<T, S> fn) {
         var newArray = new ArrayList<S>();
-        // We are executing the method from the FnFactory instance
-        arr.forEach(t -> newArray.add(fn.execute(t)));
+        // We are executing the method from the Function instance
+        arr.forEach(t -> newArray.add(fn.apply(t)));
         return newArray;
     }
     @FunctionalInterface // this doesn't do anything it is just informative.
