fffe75f2333432d84e8458d4f8e069be6a76709d
Paulo Rogerio <paulo@intranet>
2024-06-05 08:57:41 -0300

    Ajustes

--- src/HocSample.java
+++ src/HocSample.java
@@ -12,21 +12,17 @@ public class HocSample {
      Collections.sort(list, (String a, String b) -> {
         return a.compareTo(b);
      });
-     System.out.println(list); // [Apple, Banana, Grape, Orange]
-         // we are passing the array and a lambda expression as arguments to mapForEach method.
+     System.out.println(list); 
          var out = mapForEach(list, it -> it.length());
-         // This can be further simplified to "mapForEach(list, String::length);", I'm writing the expanded version for readability
-         System.out.println(out); // [6, 5, 6, 5]
+         System.out.println(out);
   }
      static <T, S> ArrayList<S> mapForEach(List<T> arr, Function<T, S> fn) {
         var newArray = new ArrayList<S>();
-        // We are executing the method from the Function instance
         arr.forEach(t -> newArray.add(fn.apply(t)));
         return newArray;
     }
-    @FunctionalInterface // this doesn't do anything it is just informative.
+    @FunctionalInterface
     public interface FnFactory<T, S> {
-        // The interface defines the contract for the anonymous class
         S execute(T it);
     }
 }
