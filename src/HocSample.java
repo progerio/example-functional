0dd2b684052897335c34724a015ea4f9419d327e
Paulo Rogerio <paulo@intranet>
2024-06-05 09:00:36 -0300

    Ajustes

--- src/HocSample.java
+++ src/HocSample.java
@@ -1,20 +1,22 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
 
 public class HocSample {
 
   public static void main(String[] args) {
-     var list = Arrays.asList("Orange", "Apple", "Banana", "Grape");
+    var list = Arrays.asList("Orange", "Apple", "Banana", "Grape");
      
-     Collections.sort(list, (String a, String b) -> {
+    Collections.sort(list, (String a, String b) -> {
         return a.compareTo(b);
      });
-     System.out.println(list); 
-         var out = mapForEach(list, it -> it.length());
-         System.out.println(out);
+    System.out.println(list); 
+    var out = mapForEach(list, it -> it.length());
+    System.out.println(out);
+
   }
      static <T, S> ArrayList<S> mapForEach(List<T> arr, Function<T, S> fn) {
         var newArray = new ArrayList<S>();
@@ -25,4 +27,9 @@ public class HocSample {
     public interface FnFactory<T, S> {
         S execute(T it);
     }
+
+    public static int sum(int a, int b) {
+      return a + b;
+    }
+
 }
